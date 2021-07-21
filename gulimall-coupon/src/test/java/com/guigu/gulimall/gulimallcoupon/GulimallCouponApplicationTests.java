3e4d7a5019ecc1948e355d9cd1d52f7b96a85976
dearcandy <18292169782@163.com>
2021-07-21 09:30:56 +0800

    项目搭建

---
+++ gulimall-coupon/src/test/java/com/guigu/gulimall/gulimallcoupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guigu.gulimall.gulimallcoupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
