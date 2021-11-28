4ac9e3d61881f2703e4b2776cbea5423a77805e4
liuhangfei <trae.liu@cloudwise.com>
2021-11-28 21:33:56 +0800

    Fixed 品牌新增基本校验完成

--- gulimall-product/src/main/java/com/guigu/gulimall/product/controller/BrandController.java
+++ gulimall-product/src/main/java/com/guigu/gulimall/product/controller/BrandController.java
@@ -1,9 +1,12 @@
 package com.guigu.gulimall.product.controller;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +18,7 @@ import com.guigu.gulimall.product.service.BrandService;
 import com.guigu.gulimall.common.utils.PageUtils;
 import com.guigu.gulimall.common.utils.R;
 
+import javax.validation.Valid;
 
 
 /**
@@ -55,8 +59,21 @@ public class BrandController {
      * 保存
      */
     @RequestMapping("/save")
-    public R save(@RequestBody BrandEntity brand){
-		brandService.save(brand);
+    public R save(@RequestBody @Validated BrandEntity brand, BindingResult result){
+        if (result.hasErrors()){
+            Map<String, String> map = new HashMap<>(16);
+            // 获取校验的错误信息
+            result.getFieldErrors().forEach(fieldError -> {
+                // 获取到的错误提示
+                String message = fieldError.getDefaultMessage();
+                // 获取字段名字
+                String field = fieldError.getField();
+                map.put(message, field);
+            });
+            return R.error(400,"提交的数据不合法").put("data", map);
+        }else{
+            brandService.save(brand);
+        }
 
         return R.ok();
     }
--- gulimall-product/src/main/java/com/guigu/gulimall/product/entity/BrandEntity.java
+++ gulimall-product/src/main/java/com/guigu/gulimall/product/entity/BrandEntity.java
@@ -6,6 +6,9 @@ import com.baomidou.mybatisplus.annotation.TableName;
 import java.io.Serializable;
 import java.util.Date;
 import lombok.Data;
+import org.hibernate.validator.constraints.URL;
+
+import javax.validation.constraints.*;
 
 /**
  * 品牌
@@ -27,10 +30,13 @@ public class BrandEntity implements Serializable {
 	/**
 	 * 品牌名
 	 */
+	@NotBlank(message = "品牌名必须提交！")
 	private String name;
 	/**
 	 * 品牌logo地址
 	 */
+	@NotEmpty(message = "品牌logo地址不能为空")
+	@URL(message = "logo必须是一个合法的url地址！")
 	private String logo;
 	/**
 	 * 介绍
@@ -43,10 +49,14 @@ public class BrandEntity implements Serializable {
 	/**
 	 * 检索首字母
 	 */
+	@NotEmpty(message = "检索首字母不能为空")
+	@Pattern(regexp = "/^[a-zA-Z]$/", message = "检索首字母必须是另外一个字母")
 	private String firstLetter;
 	/**
 	 * 排序
 	 */
+	@NotNull
+	@Min(value = 0, message = "排序字段必须大于0")
 	private Integer sort;
 
 }
