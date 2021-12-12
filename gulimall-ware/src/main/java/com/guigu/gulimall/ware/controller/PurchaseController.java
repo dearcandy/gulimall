dbd808eddad5e7176aa3c7089e289e7f626d921d
liuhangfei <trae.liu@cloudwise.com>
2021-12-12 17:42:02 +0800

    Feature  仓库模糊查询 采购需求 采购单模糊查询

--- gulimall-ware/src/main/java/com/guigu/gulimall/ware/controller/PurchaseController.java
+++ gulimall-ware/src/main/java/com/guigu/gulimall/ware/controller/PurchaseController.java
@@ -4,11 +4,7 @@ import java.util.Arrays;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.guigu.gulimall.ware.entity.PurchaseEntity;
 import com.guigu.gulimall.ware.service.PurchaseService;
@@ -29,6 +25,14 @@ import com.guigu.gulimall.common.utils.R;
 public class PurchaseController {
     @Autowired
     private PurchaseService purchaseService;
+
+    // ware/purchase/unreceive/list
+
+    @GetMapping("/unreceive/list")
+    public R unReceiveList(@RequestParam Map<String, Object> params){
+        PageUtils page = purchaseService.queryPageunReceive(params);
+        return R.ok().put("page", page);
+    }
 
     /**
      * 列表
